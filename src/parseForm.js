4826360774f240f753f63804491a43e42879dd15
Rich Churcher <rich.churcher@gmail.com>
2016-11-26 17:49:50 +1300

    parseForm aggregates answers by column

--- src/parseForm.js
+++ src/parseForm.js
@@ -1,3 +1,22 @@
-export default function () {
-  return null
+export default function parseForm (raw) {
+  const answers = raw.rowData.slice(1)
+  return {
+    questions:
+      raw.rowData[0].values.map((col, i) => {
+        const question = getQuestion(col)
+        return Object.assign(question, {
+          answers: answers.map(row =>
+            row.values[i].userEnteredValue.stringValue)
+        })
+      })
+  }
+}
+
+export function getQuestion (columnHeader) {
+  const parts = /([\d]*\.?[\d]*) (.*)/.exec(columnHeader.userEnteredValue.stringValue)
+  if (parts) {
+    const [_, designator, title] = parts
+    return { designator, title }
+  }
+  return { title: columnHeader }
 }
--- test/parseForm.test.js
+++ test/parseForm.test.js
@@ -1,6 +1,51 @@
 import test from 'ava'
 import parseForm from '../src/parseForm'
 
-test('parseForm', t => {
-  t.pass()
+test('parseForm aggregates answers by column', t => {
+  const raw = {
+    rowData: [
+      {
+        values: [
+          { userEnteredValue: { stringValue: '1.0 Who flung dung?' } },
+          { userEnteredValue: { stringValue: '1.2 GitHub name' } },
+          { userEnteredValue: { stringValue: '5.2 Was dung flung at all?' } }
+        ]
+      },
+      {
+        values: [
+          { userEnteredValue: { stringValue: 'I did' } },
+          { userEnteredValue: { stringValue: 'richchurcher' } },
+          { userEnteredValue: { stringValue: 'Yes' } }
+        ]
+      },
+      {
+        values: [
+          { userEnteredValue: { stringValue: 'They did' } },
+          { userEnteredValue: { stringValue: 'someone' } },
+          { userEnteredValue: { stringValue: 'Yes' } }
+        ]
+      }
+    ]
+  }
+  const expected = {
+    questions: [
+      {
+        designator: '1.0',
+        title: 'Who flung dung?',
+        answers: ['I did', 'They did']
+      },
+      {
+        designator: '1.2',
+        title: 'GitHub name',
+        answers: ['richchurcher', 'someone']
+      },
+      {
+        designator: '5.2',
+        title: 'Was dung flung at all?',
+        answers: ['Yes', 'Yes']
+      }
+    ]
+  }
+  const actual = parseForm(raw)
+  t.deepEqual(expected, actual)
 })
