4866128f834ea420c8c3768e49eceeffc23e346d
Rich Churcher <rich.churcher@gmail.com>
2016-11-27 20:19:33 +1300

    Add tracking

--- src/parseForm.js
+++ src/parseForm.js
@@ -1,22 +1,55 @@
-export function parseForm (raw) {
-  const answers = raw.rowData.slice(1)
-  return {
-    questions:
-      raw.rowData[0].values.map((col, i) => {
-        const question = getQuestion(col)
-        return Object.assign(question, {
-          answers: answers.map(row =>
-            row.values[i].userEnteredValue.stringValue)
-        })
-      })
+export function parseForm (raw, options) {
+  const rawAnswers = raw.rowData.slice(1)
+  const headers = raw.rowData[0].values.map((header, col) => getQuestion(header, col))
+  const answers = headers.map((h, col) => getAnswers(rawAnswers, col))
+  const questions = headers.map(({designator, title}, col) => {
+    const out = { designator, title, answers: answers[col] }
+    return out
+  })
+  if (options) {
+    if (options.trackByDesignators) {
+      const trackingAnswers = headers
+        .filter(({designator}) => options.trackByDesignators.includes(designator))
+        .map(header => getTrackingAnswers(header, rawAnswers))
+      return {
+        questions: questions.map(question => attachTrackingAnswers(question, trackingAnswers))
+      }
+    }
   }
+  return { questions }
 }
 
-export function getQuestion ({ userEnteredValue: {stringValue} }) {
+export function getQuestion ({ userEnteredValue: {stringValue} }, col) {
   const parts = /^([\d]*\.?[\d]*) (.*)$/.exec(stringValue)
   if (parts) {
     const [_, designator, title] = parts
-    return { designator, title }
+    return { designator, title, col }
   }
-  return { title: stringValue }
+  return { title: stringValue, col }
+}
+
+export function getAnswers (rawAnswers, col) {
+  return rawAnswers.map(row =>
+    row.values[col].userEnteredValue.stringValue)
+}
+
+export function getTrackingAnswers ({title, col}, rawAnswers) {
+  return rawAnswers.map(row => {
+    const answer = {}
+    answer[title] = row.values[col].userEnteredValue.stringValue
+    return answer
+  })
+}
+
+export function attachTrackingAnswers ({designator, title, answers}, trackingAnswers) {
+  const answersWithTracking = answers.map((answer, col) => {
+    const out = { answer }
+    trackingAnswers.forEach(a => Object.assign(out, a[col]))
+    return out
+  })
+  return {
+    designator,
+    title,
+    answers: answersWithTracking
+  }
 }
--- test/parseForm.test.js
+++ test/parseForm.test.js
@@ -1,37 +1,13 @@
 import test from 'ava'
-import {getQuestion, parseForm} from '../src/parseForm'
+import {getQuestion, getTrackingAnswers, parseForm} from '../src/parseForm'
 
-test('getQuestion sets designator and title correctly', t => {
-  const raw = { userEnteredValue: { stringValue: '1111.911 Who flung dung?' } }
-  const expected = {
-    designator: '1111.911',
-    title: 'Who flung dung?'
-  }
-  const actual = getQuestion(raw)
-  t.deepEqual(expected, actual)
-})
-
-test('getQuestion does not set designator if malformed', t => {
-  const raw = { userEnteredValue: { stringValue: '1Afb3,9 Who flung dung?' } }
-  const expected = { title: '1Afb3,9 Who flung dung?' }
-  const actual = getQuestion(raw)
-  t.deepEqual(expected, actual)
-})
-
-test('getQuestion does not set designator if missing', t => {
-  const raw = { userEnteredValue: { stringValue: 'Who flung dung?' } }
-  const expected = { title: 'Who flung dung?' }
-  const actual = getQuestion(raw)
-  t.deepEqual(expected, actual)
-})
-
-test('parseForm aggregates answers by column', t => {
-  const raw = {
+test.beforeEach(t => {
+  t.context.raw = {
     rowData: [
       {
         values: [
           { userEnteredValue: { stringValue: '1.0 Who flung dung?' } },
-          { userEnteredValue: { stringValue: '1.2 GitHub name' } },
+          { userEnteredValue: { stringValue: '1.2 GitHub' } },
           { userEnteredValue: { stringValue: '5.2 Was dung flung at all?' } }
         ]
       },
@@ -51,6 +27,34 @@ test('parseForm aggregates answers by column', t => {
       }
     ]
   }
+})
+
+test('getQuestion sets designator and title correctly', t => {
+  const raw = { userEnteredValue: { stringValue: '1111.911 Who flung dung?' } }
+  const expected = {
+    designator: '1111.911',
+    title: 'Who flung dung?',
+    col: 0
+  }
+  const actual = getQuestion(raw, 0)
+  t.deepEqual(expected, actual)
+})
+
+test('getQuestion does not set designator if malformed', t => {
+  const raw = { userEnteredValue: { stringValue: '1Afb3,9 Who flung dung?' } }
+  const expected = { title: '1Afb3,9 Who flung dung?', col: 0 }
+  const actual = getQuestion(raw, 0)
+  t.deepEqual(expected, actual)
+})
+
+test('getQuestion does not set designator if missing', t => {
+  const raw = { userEnteredValue: { stringValue: 'Who flung dung?' } }
+  const expected = { title: 'Who flung dung?', col: 0 }
+  const actual = getQuestion(raw, 0)
+  t.deepEqual(expected, actual)
+})
+
+test('parseForm aggregates answers by column', t => {
   const expected = {
     questions: [
       {
@@ -60,7 +64,7 @@ test('parseForm aggregates answers by column', t => {
       },
       {
         designator: '1.2',
-        title: 'GitHub name',
+        title: 'GitHub',
         answers: ['richchurcher', 'someone']
       },
       {
@@ -70,6 +74,67 @@ test('parseForm aggregates answers by column', t => {
       }
     ]
   }
-  const actual = parseForm(raw)
+  const actual = parseForm(t.context.raw)
+  t.deepEqual(expected, actual)
+})
+
+test('parseForm tracks by designators', t => {
+  const expected = {
+    questions: [
+      {
+        designator: '1.0',
+        title: 'Who flung dung?',
+        answers: [
+          { 'GitHub': 'richchurcher', answer: 'I did' },
+          { 'GitHub': 'someone', answer: 'They did' }
+        ]
+      },
+      {
+        designator: '1.2',
+        title: 'GitHub',
+        answers: [
+          { 'GitHub': 'richchurcher', answer: 'richchurcher' },
+          { 'GitHub': 'someone', answer: 'someone' }
+        ]
+      },
+      {
+        designator: '5.2',
+        title: 'Was dung flung at all?',
+        answers: [
+          { 'GitHub': 'richchurcher', answer: 'Yes' },
+          { 'GitHub': 'someone', answer: 'Yes' }
+        ]
+      }
+    ]
+  }
+  const actual = parseForm(t.context.raw, {
+    trackByDesignators: ['1.2']
+  })
+  t.deepEqual(expected, actual)
+})
+
+test('getTrackingAnswers returns question:answer couplets', t => {
+  const trackByQuestion = { title: 'GitHub', col: 1 }
+  const answers = [
+    {
+      values: [
+        { userEnteredValue: { stringValue: 'I did' } },
+        { userEnteredValue: { stringValue: 'richchurcher' } },
+        { userEnteredValue: { stringValue: 'Yes' } }
+      ]
+    },
+    {
+      values: [
+        { userEnteredValue: { stringValue: 'They did' } },
+        { userEnteredValue: { stringValue: 'someone' } },
+        { userEnteredValue: { stringValue: 'Yes' } }
+      ]
+    }
+  ]
+  const expected = [
+    { 'GitHub': 'richchurcher' },
+    { 'GitHub': 'someone' }
+  ]
+  const actual = getTrackingAnswers(trackByQuestion, answers)
   t.deepEqual(expected, actual)
 })
